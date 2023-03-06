2548b4c073a434b730a07345e80b83f59673edb5
Ankesh <banerjeeankesh@gmail.com>
2023-03-06 13:21:00 +0530

    Added files

---
+++ Questions/Graphs/G-17_isGraphBipartite.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    bool check  (int start, vector<vector<int>>& graph, vector<int> &color){
+        queue <int> q;
+        q.push(start);
+
+        while (!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for (auto it : graph[node]){
+                // if not colored
+                if(color[it] == -1){
+                    color[it] = !color[node];
+                    q.push(it);
+                }
+                // adjacing node having same color as that of current node
+                else if (color[it] == color[node])return false;
+            }
+        }
+        return true;
+    }
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int v = graph.size();
+        vector <int> color (v, -1);
+        for (int i=0; i<v; i++){
+            // if not colored
+            if (color[i] == -1){
+                if (!check(i, graph, color)) return false;
+            }
+        }
+        return true;
+    }
+};
