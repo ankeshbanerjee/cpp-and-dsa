af923032bdafbc60a296b458d116418bcfb29b48
Ankesh <banerjeeankesh@gmail.com>
2023-05-04 13:55:46 +0530

    Added files

--- 30 Dynamic Programming/10_minimumSumPath.cpp
+++ 30 Dynamic Programming/10_minimumSumPath.cpp
@@ -34,3 +34,59 @@ public:
         return f(m-1, n-1, dp, grid);
     }
 };
+// TC = O(m*n) 
+// SC = O(path length) + dp array space = O((m-1) + (n-1)) + O(m*n)
+
+
+// tabulation
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp (m, vector<int> (n, 0));
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (i==0 && j==0) dp[i][j] = grid[i][j];
+                else{
+                    int up, left;
+                    up = left = 1e9;
+                    if (i>0) up = grid[i][j] + dp[i-1][j];
+                    if (j>0) left = grid[i][j] + dp[i][j-1];
+                    dp[i][j] = min (up, left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+// TC = O(m*n)
+// SC = O(m*n)
+
+
+// space optimization
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> prev (n, 0);
+        for (int i=0; i<m; i++){
+            vector<int> curr (n, 0);
+            for (int j=0; j<n; j++){
+                if (i==0 && j==0) curr[j] = grid[i][j];
+                else{
+                    int up, left;
+                    up = left = 1e9;
+                    if (i>0) up = grid[i][j] + prev[j];
+                    if (j>0) left = grid[i][j] + curr[j-1];
+                    curr[j] = min (up, left);
+                }
+            }
+            prev = curr;
+        }
+        return prev[n-1];
+    }
+};
+// TC = O(m*n)
+// SC = O(n)
